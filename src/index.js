beac202ee46cc22e2862bab987be65051d0920a6
SantoshKumarRavi <drsanthosh1997@gmail.com>
2022-12-07 07:20:00 +0530

    env added for secret keys

--- src/index.js
+++ src/index.js
@@ -3,19 +3,14 @@ const app = express()
 const port = 8081
 const {google} = require('googleapis');
 const OAuth2 = google.auth.OAuth2;
-
+require("dotenv").config()
 app.get('/meet', (req, res) => {
-
-
-
 async function main () {
-let ClientId="197291748005-usi6ej087djrpcsqphpfrj5bucvskvrm.apps.googleusercontent.com"
-let ClientSecret="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"
-let redirecturl="http://localhost:8081/"
-const oauth2Client =await new OAuth2(
-    ClientId,
-    ClientSecret,
-    redirecturl
+
+  const oauth2Client =await new OAuth2(
+    process.env.ClientId,
+    process.env.ClientSecret,
+    process.env.redirecturl
 );
 const scopes = [
     'https://www.googleapis.com/auth/calendar',
