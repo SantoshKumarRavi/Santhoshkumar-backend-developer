09f43b1d3482ce8f87958412ee240e0c88c7c9a0
SantoshKumarRavi <drsanthosh1997@gmail.com>
2022-12-07 14:28:57 +0530

    api end points changed to Rest api

--- src/index.js
+++ src/index.js
@@ -2,90 +2,40 @@ const express = require('express')
 const app = express()
 const port = 8081
 const {google} = require('googleapis');
-// const fs = require('fs').promises;
 var {OAuth2Client} = require('google-auth-library');
-// var auth = new googleAuth();
-// console.log(googleAuth)
-// const OAuth2 = auth.OAuth2;//chaning library 
-
-// const OAuth2 = google.auth.OAuth2;
 require("dotenv").config()
-const {authenticate} = require('@google-cloud/local-auth');
-// const path = require('path');
-// const process = require('process');
-// const CREDENTIALS_PATH = path.join(process.cwd(), "src/credential.json");
 const oauth2Client = new OAuth2Client(
   process.env.CLIENTID,
   process.env.CLIENTSECRET,
   process.env.REDIRECTURL
 );
-// google.options({ auth: oauth2Client })
 const scopes = [
   'https://www.googleapis.com/auth/calendar',
   'https://www.googleapis.com/auth/calendar.acls'
 ];
-app.get('/scheduless', (req, res) => {
+app.get('/rest/v1/calendar/init', (req, res) => {
 async function main () {
   const url = await oauth2Client.generateAuthUrl({
     access_type: 'offline',
     scope: scopes,
-    include_granted_scopes:true,
-    // prompt: 'consent',
-    // response_type:"code",
-    // grant_type: 'authorization_code'
+    include_granted_scopes:true
   });
-// res.writeHead(307, { "Location": url });
 res.redirect(307 ,url) //307 temp ... 301 permananet
   }
   main().catch(console.error);
 })
 
-app.get("/redirect",async(req, res) => {
-  // const updatingrefresh=(
-  // async function gettingToken (){
-    const {code}=(req.query)
+app.get("/rest/v1/calendar/redirect ",async(req, res) => {
+
+    const {code}=(req.query) //hard_coded event
     const {tokens} =await oauth2Client.getToken(code)
     console.log("withour accesstokens ",tokens)
     await oauth2Client.setCredentials(tokens)
-    // return await updatingrefresh
-  // }
-  // )()
-  
-  // const accesstokens=await oauth2Client.getAccessToken()
-  // const refersh =await oauth2Client.refreshAccessToken()
-  // const token_info =await oauth2Client.getTokenInfo()
-// res.send("event updated")
-// return
-//   const client = await google.auth.getClient()
-
-// client.on('tokens', (tokens) => {
-//   if (tokens.refresh_token) {
-//     // store the refresh_token in my database!
-//     console.log(tokens);
-//   }
-//   // console.log(tokens);
-// });
-  // console.log("withour refersh ",refersh)
-  // console.log("withour token_info ",token_info)
-  // async function quickstart() {
-  //   const localAuth = await authenticate({
-  //     scopes:scopes,
-  //     keyfilePath:CREDENTIALS_PATH,
-  //   });
-  //   console.log('Tokens:', localAuth.credentials);
-  // }
-  // quickstart();
-
-  
-  // callback(oauth2Client)
-  //getting refresh token implement(as of now using acces token)
-    //setting events
-  // function callback(oauth2Client){
     const calendar=google.calendar({version:'v3',auth:oauth2Client})
     let event={
-      summary:"youth india asignment",// title 
+      summary:"youth india asignment",
       location:"TN", 
-      description:"backend project youth india", //description
+      description:"backend project youth india",
       start:{dateTime:'2022-12-07T04:30:00.000Z'},//10am
       end:{dateTime:'2022-12-07T05:30:00.000Z'},//11am
       timeZone:'Asia/Kolkata',
@@ -100,42 +50,18 @@ app.get("/redirect",async(req, res) => {
           }
       }
     }
-    // calendar.freebusy.query({
-    //   resource:{
-    //       timeMin:1670387400000, //10am
-    //       timeMax:1670391000000, //11am
-    //       timeZone:'Asia/Kolkata',
-    //       items:[{id:'primary'}],
-    //   },
-    // },(err,freebusyResult)=>{
-      // if(err){
-      //   console.error("before free busy error")
-      //     return console.error("free busy ====>",err)
-      // }
-      // const eventArr=freebusyResult.data.calendars.primary.busy
-      // if(eventArr.length==0){
-          // console.log("here wauit from if part,,,,,,,")
    
-          calendar.events.insert({  auth:oauth2Client,calendarId:'primary',conferenceDataVersion: 1,resource:event},(err,eventData)=>{
+    calendar.events.insert({  auth:oauth2Client,calendarId:'primary',conferenceDataVersion: 1,resource:event},(err,eventData)=>{
       if(err){
         res.send(`Error while creating calendar`)
         console.log(err)
            return
       }
-      // console.log("before event update")
       if(eventData){
         res.send(`event data is added`)
       }
     })
     
-    // }
-    // else{
-    //   res.send(`event data is not added, he/she is busy at this schedule`)
-    // }
-    // }
-    // )
-  // }    
-    // .catch.....
 })
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
