68bb2554afbf816fc13b109a1e633da4258b5289
SantoshKumarRavi <drsanthosh1997@gmail.com>
2022-12-07 08:32:31 +0530

    added redirect url in oAuth and implemented of getting access token

--- src/index.js
+++ src/index.js
@@ -4,13 +4,20 @@ const port = 8081
 const {google} = require('googleapis');
 const OAuth2 = google.auth.OAuth2;
 require("dotenv").config()
-app.get('/meet', (req, res) => {
+
+
+// app.get('/', (req, res) => {
+//   console.log(req.query)
+// res.send("working , ur authorized")
+// })
+
+app.get('/meets', (req, res) => {
 async function main () {
 
   const oauth2Client =await new OAuth2(
-    process.env.ClientId,
-    process.env.ClientSecret,
-    process.env.redirecturl
+    process.env.CLIENTID,
+    process.env.CLIENTSECRET,
+    process.env.REDIRECTURL
 );
 const scopes = [
     'https://www.googleapis.com/auth/calendar',
@@ -24,13 +31,25 @@ const scopes = [
   });
 //   const {tokens} =await oauth2Client.getToken(code)
 //   console.log(tokens)
-console.log("working...",url)
+// console.log("working...",url)
 // res.writeHead(301,{Location:url})
-res.redirect(301,url)
+res.redirect(301 ,url) //307 temp ... 301 permananet
+// res.send("hi get")
   }
   main().catch(console.error);
 })
 
+app.get("/redirect",async(req, res) => {
+  const oauth2Client =await new OAuth2(
+    process.env.CLIENTID,
+    process.env.CLIENTSECRET,
+    process.env.REDIRECTURL
+);
+  const {code}=(req.query)
+  const {tokens} =await oauth2Client.getToken(code)
+  console.log(tokens)
+  res.send(`working refiertct , ur authorized here is the token}`)
+})
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
